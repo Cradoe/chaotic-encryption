4590c317f58b468d0256b58f12bceeb0b8353069
Cradoe <me@ismailobadimu.com>
2021-06-24 19:11:26 +0100

    Update

---
+++ open-image.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <?php include "includes/header-resources.php"; ?>
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="horizontal-layout horizontal-menu content-left-sidebar navbar-floating footer-static  " data-open="hover" data-menu="horizontal-menu" data-col="content-left-sidebar">
+
+    <!-- BEGIN: Header-->
+    <?php include "includes/header-nav.php"; ?>
+    <!-- END: Header-->
+    <style>
+        .app-content {
+            padding: calc(2rem + 4.45rem + 1.3rem) 2rem 0 !important;
+        }
+    </style>
+    <!-- BEGIN: Content-->
+    <div class="app-content content chat-application">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-area-wrapper container-xxl p-0">
+            <div class="row">
+                <div class="card">
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+    </div>
+    <div class="sidenav-overlay"></div>
+    <div class="drag-target"></div>
+
+    <!-- BEGIN: Footer-->
+    <?php include "includes/footer.php"; ?>
+    <!-- END: Footer-->
+
+    <?php include "includes/footer-resources.php"; ?>
+
+</body>
+<!-- END: Body-->
+
+</html>
