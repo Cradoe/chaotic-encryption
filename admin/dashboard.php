9b7acd6eccff19ef3628bb5ba3a555a939b2c5bd
Cradoe <me@ismailobadimu.com>
2021-06-26 07:12:37 +0100

    Update

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -23,61 +23,70 @@
     <!-- BEGIN: Content-->
     <div class="app-content content chat-application">
         <div class="row match-height">
-            <!-- Greetings Card starts -->
-            <div class="col-lg-6 col-md-12 col-sm-12">
-                <div class="card card-congratulations">
-                    <div class="card-body text-center">
-                        <img src="../app-assets/images/elements/decore-left.png" class="congratulations-img-left" alt="card-img-left" />
-                        <img src="../app-assets/images/elements/decore-right.png" class="congratulations-img-right" alt="card-img-right" />
-                        <div class="avatar avatar-xl bg-primary shadow">
-                            <div class="avatar-content">
-                                <i data-feather="award" class="font-large-1"></i>
+
+
+            <div class="col-lg-3 col-sm-6 col-12">
+                <a href="new-mission">
+                    <div class="card bg-primary text-white">
+                        <div class="card-header flex-column align-items-start ">
+                            <div class="avatar bg-light-warning p-50 m-0">
+                                <div class="avatar-content">
+                                    <i data-feather='smile' class="font-medium-5"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="text-center">
-                            <h1 class="mb-1 text-white">Congratulations John,</h1>
-                            <p class="card-text m-auto w-75">
-                                You have done <strong>57.6%</strong> more sales today. Check your new badge in your profile.
-                            </p>
+                            <h2 class="fw-bolder mt-1 text-warning">New Mission</h2>
+                            <p class="card-text">Choose teams members and oga</p>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
-            <!-- Greetings Card ends -->
 
-            <!-- Subscribers Chart Card starts -->
+
             <div class="col-lg-3 col-sm-6 col-12">
                 <div class="card">
-                    <div class="card-header flex-column align-items-start pb-0">
+                    <div class="card-header flex-column align-items-start ">
                         <div class="avatar bg-light-primary p-50 m-0">
                             <div class="avatar-content">
                                 <i data-feather="users" class="font-medium-5"></i>
                             </div>
                         </div>
                         <h2 class="fw-bolder mt-1">92.6k</h2>
-                        <p class="card-text">Subscribers Gained</p>
+                        <p class="card-text">Soldiers</p>
                     </div>
-                    <div id="gained-chart"></div>
                 </div>
             </div>
-            <!-- Subscribers Chart Card ends -->
 
-            <!-- Orders Chart Card starts -->
             <div class="col-lg-3 col-sm-6 col-12">
                 <div class="card">
-                    <div class="card-header flex-column align-items-start pb-0">
+                    <div class="card-header flex-column align-items-start ">
                         <div class="avatar bg-light-warning p-50 m-0">
                             <div class="avatar-content">
                                 <i data-feather="package" class="font-medium-5"></i>
                             </div>
                         </div>
                         <h2 class="fw-bolder mt-1">38.4K</h2>
-                        <p class="card-text">Orders Received</p>
+                        <p class="card-text">Missions</p>
                     </div>
-                    <div id="order-chart"></div>
                 </div>
             </div>
-            <!-- Orders Chart Card ends -->
+
+            <div class="col-lg-3 col-sm-6 col-12">
+                <div class="card">
+                    <div class="card-header flex-column align-items-start ">
+                        <div class="avatar bg-light-warning p-50 m-0">
+                            <div class="avatar-content">
+                                <i data-feather="package" class="font-medium-5"></i>
+                            </div>
+                        </div>
+                        <h2 class="fw-bolder mt-1">38.4K</h2>
+                        <p class="card-text">Completed Missions</p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
         </div>
         <div class="row">
             <div class="col-12 col-md-6">
@@ -92,7 +101,7 @@
                                     <th></th>
                                     <th>Mission Code</th>
                                     <th>Members</th>
-                                    <th>Captain</th>
+                                    <th>Oga</th>
                                     <th>Date Started</th>
                                     <th class="cell-fit">Actions</th>
                                 </tr>
@@ -105,7 +114,7 @@
                                     <td>Adewale Idris</td>
                                     <td>1</td>
                                     <td>
-                                        <a href="mission" class="btn btn-primary btn-sm">View Members</a>
+                                        <a href="mission" class="btn btn-primary btn-sm">View</a>
                                     </td>
                                 </tr>
                             </tbody>
@@ -125,7 +134,7 @@
                                     <th></th>
                                     <th>Mission Code</th>
                                     <th>Members</th>
-                                    <th>Captain</th>
+                                    <th>Oga</th>
                                     <th>Date Started</th>
                                     <th class="cell-fit">Actions</th>
                                 </tr>
@@ -138,7 +147,7 @@
                                     <td>1</td>
                                     <td>1</td>
                                     <td>
-                                        <a href="completed-mission" class="btn btn-primary btn-sm">View Members</a>
+                                        <a href="completed-mission" class="btn btn-primary btn-sm">View</a>
                                     </td>
                                 </tr>
                             </tbody>
